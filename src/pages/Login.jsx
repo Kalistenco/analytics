c8333ccde0769e2110dd46cd663dd2546a019c25
kalistenco <martinkalistenco@gmail.com>
2023-06-25 21:47:45 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
     const handleLogin = async () => {
         try {
-            const res = await fetch("http://localhost:5000/login", {
+            const res = await fetch("https://analytics-backend-nine.vercel.app/login", {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
